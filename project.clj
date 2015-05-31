d4fc0d8fa6d3175bbf3c96e7708481bf17a2d289
Jozef Wagner <jozef.wagner@gmail.com>
2015-05-31 21:10:46 +0200

    bump to 0.5

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject dunaj-lite-starter "0.4.0"
+(defproject dunaj-lite-starter "0.5.0"
   :description "A starter leiningen project for Dunaj lite"
   :url "https://github.com/dunaj-project/dunaj-lite-starter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.dunaj/dunaj-lite "0.4.0"]
-                 [org.clojure/clojure "1.7.0-beta1"]])
+  :dependencies [[org.dunaj/dunaj-lite "0.5.0"]
+                 [org.clojure/clojure "1.7.0-RC1"]])
